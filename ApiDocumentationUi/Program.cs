5c4b7b979496029304db218531b8c03fac72a777
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2025-01-08 20:58:04 +0100

    add scalar options

--- ApiDocumentationUi/Program.cs
+++ ApiDocumentationUi/Program.cs
@@ -13,7 +13,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
     // /scalar/v1
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    {
+        // options.Theme = ScalarTheme.DeepSpace;
+    });
 
     // /nswag
     NSwagApplicationBuilderExtensions.UseSwaggerUi(app, options =>
@@ -36,12 +39,12 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-var apiUsers = app.MapGroup("api/users/");
+var apiUsers = app.MapGroup("api/users/").WithTags("users");
 apiUsers.MapPost("", CreateUserAsync);
 apiUsers.MapGet("", FetchUsersAsync);
 apiUsers.MapGet("{id:int}", FetchUserAsync);
-apiUsers.MapPut("{id:int}", UpdateUserAsync);
-apiUsers.MapDelete("{id:int}", DeleteUserAsync);
+apiUsers.MapPut("{id:int}", UpdateUserAsync).WithSummary("Update user");
+apiUsers.MapDelete("{id:int}", DeleteUserAsync).WithDescription("Deletes user by given ID");
 
 app.Run();
 
@@ -54,15 +57,21 @@ async Task<Created> CreateUserAsync(CancellationToken cancellationToken)
     return TypedResults.Created();
 }
 
-async Task<Ok<UserDto[]>> FetchUsersAsync([FromQuery] string name, [FromQuery] Gender? gender, CancellationToken cancellationToken)
+async Task<Ok<UserDto[]>> FetchUsersAsync(
+    [FromQuery] string name,
+    [FromQuery] Gender? gender,
+    CancellationToken cancellationToken)
 {
     await Task.Delay(1_000, cancellationToken);
     var faker = new Faker();
-    var userDto = new UserDto(faker.Random.Int(1), faker.Person.FirstName, gender ?? faker.PickRandom<Gender>(), faker.Random.Int(0, 100));
+    var userDto = new UserDto(faker.Random.Int(1), faker.Person.FirstName, gender ?? faker.PickRandom<Gender>(),
+        faker.Random.Int(0, 100));
     return TypedResults.Ok(new[] { userDto });
 }
 
-async Task<Results<Ok<UserDto>, NotFound>> FetchUserAsync(int id, CancellationToken cancellationToken)
+async Task<Results<Ok<UserDto>, NotFound>> FetchUserAsync(
+    [FromRoute] int id,
+    CancellationToken cancellationToken)
 {
     await Task.Delay(1_000, cancellationToken);
     var faker = new Faker();
@@ -70,13 +79,18 @@ async Task<Results<Ok<UserDto>, NotFound>> FetchUserAsync(int id, CancellationTo
     return TypedResults.Ok(userDto);
 }
 
-async Task<Results<NoContent, NotFound>> UpdateUserAsync(int id, CancellationToken cancellationToken)
+async Task<Results<NoContent, NotFound>> UpdateUserAsync(
+    [FromRoute] int id,
+    [FromBody] PutUserDto dto,
+    CancellationToken cancellationToken)
 {
     await Task.Delay(1_000, cancellationToken);
     return TypedResults.NoContent();
 }
 
-async Task<Results<NoContent, NotFound>> DeleteUserAsync(int id, CancellationToken cancellationToken)
+async Task<Results<NoContent, NotFound<NotFoundResponseBody>>> DeleteUserAsync(
+    [FromRoute] int id,
+    CancellationToken cancellationToken)
 {
     await Task.Delay(1_000, cancellationToken);
     return TypedResults.NoContent();
@@ -89,3 +103,7 @@ public enum Gender
 }
 
 public sealed record UserDto(int Id, string Name, Gender Gender, int? Age);
+
+public sealed record PutUserDto(string Name, Gender Gender, int? Age);
+
+public sealed record NotFoundResponseBody(string Message);
